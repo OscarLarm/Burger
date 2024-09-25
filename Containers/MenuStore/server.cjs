58225c034e73518ca0bfb1e6749b563d08bbc818
git-hub-oscar <118494885+git-hub-oscar@users.noreply.github.com>
2024-09-25 13:23:45 +0200

    Add paths for different types for rest-api

--- Containers/MenuStore/server.cjs
+++ Containers/MenuStore/server.cjs
@@ -4,32 +4,49 @@ const express = require('express')
 
 app = express();
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/MenuStore');
-    // const Food = mongoose.model('Model', {}, 'FoodItems');
-    const foodSchema = new mongoose.Schema({
-        type: String,
-        name: String
-    });
-    const FoodType = mongoose.model("Food", foodSchema, 'FoodItems');
-    const food = await FoodType.find({type:'burger'});
-    console.log();
-
-    food.forEach(element => {
-        console.log(element['name'])
-    });
-}
-
-app.get('/test', (req, res) => {
-    console.log('test success')
-    res.send('testest')
-})
+express.json();
 
 app.listen(3000, () => {
     console.log('Server running on port 3000')
 });
 
-app.get('/', (req, res) => {
-    console.log('test')
-    res.send('hey');
+
+// const Food = mongoose.model('Model', {}, 'FoodItems');
+const foodSchema = new mongoose.Schema({
+    id: String,
+    type: String,
+    name: String,
+    ingredients: [{type: String}]
 });
+
+
+//console.log(food);
+
+
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/MenuStore');
+    console.log('Connected to MenuStore')
+    const FoodType = mongoose.model("Food", foodSchema, 'FoodItems');
+    const allFood = await FoodType.find({});
+    const burgers = await FoodType.find({type: "burger"});
+    const sides = await FoodType.find({type: "sides"});
+    // Loop through to post each element
+    // food.forEach(element => {
+    //     console.log(element['name'])
+    // });
+
+    app.get('/', (req, res) => {
+        console.log("Getting all database data...")
+        res.send(allFood);
+    })
+
+    app.get('/burgers', (req, res) => {
+        console.log("Getting all database 'burger' data...")
+        res.send(burgers);
+    })
+    app.get('/sides', (req, res) => {
+        console.log("Getting all database 'sides' data...")
+        res.send(sides);
+    })
+}
