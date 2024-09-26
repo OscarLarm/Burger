d40ec3ac6a2a382013025d9e981a21f260e6e8f4
Maximilian Åberg <maximilian.aberg4@gmail.com>
2024-09-26 17:09:42 +0200

    updated engineering diary

---
+++ Containers/BurgerOrderer/index.cjs
@@ -0,0 +1,81 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const PORT = 3000;
+
+// Statiska burgare
+const staticBurgers = [
+    { name: 'fettburgare' },
+    { name: 'gnuttburgare' },
+    { name: 'isterburgare' }
+];
+
+// Hämta burgare
+function getBurgers() {
+    return staticBurgers;
+}
+
+// Rendera startsida
+function renderFrontpage() {
+    let pg = "<h1>Welcome to BurgerOrderer</h1>";
+    pg += "<p><ul>";
+
+    getBurgers().forEach(b => {
+        pg += "<li>" + b.name + "</li>";
+    });
+
+    pg += "</ul></p>";
+    return pg;
+}
+
+// Rendera beställningssida
+function renderOrderingPage(burgerName, args) {
+    return `ordered ${burgerName}`;
+}
+
+// Bas-URL för KitchenView
+const baseURL = `http://${process.env.KITCHENVIEW_HOST || 'localhost:5000'}`;
+
+// Skapa URL för beställning
+function makeURL(burgerName) {
+    return `${baseURL}/buy/${burgerName}`;
+}
+
+// Lägg till alternativ i URL
+function addOptions(url, args) {
+    const query = Object.keys(args).map(arg => `${arg}=${args[arg]}`).join('&');
+    return query.length > 0 ? `${url}?${query}` : url;
+}
+
+// Skicka beställning till KitchenView
+function sendToKitchen(burgerName, args) {
+    let requrl = makeURL(burgerName);
+    requrl = addOptions(requrl, args);
+
+    console.log('Using KitchenView URL: ' + requrl);
+    axios.get(requrl)
+        .then(response => {
+            console.log('Order sent successfully');
+        })
+        .catch(error => {
+            console.error('Error sending order:', error);
+        });
+}
+
+// Hantera startsida
+app.get('/', (req, res) => {
+    res.send(renderFrontpage());
+});
+
+// Hantera burgarbeställning
+app.get('/buy/:burgerName', (req, res) => {
+    const burgerName = req.params.burgerName;
+    console.log('Placing an order on ' + burgerName);
+    sendToKitchen(burgerName, req.query);
+    res.send(renderOrderingPage(burgerName, req.query));
+});
+
+// Starta servern
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
